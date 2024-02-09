ba6ea735fd997faefa560a850515b70406523a13
Prashant Sharma <97109392+Prashant419@users.noreply.github.com>
2024-02-09 17:24:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+ constructor(width, height) {
+   if (width <= 0 || height <= 0) {
+     throw new Error("Width and height must be positive integers.");
+   }
+   this._width = width;
+   this._height = height;
+ }
 
-class Square extends Animal {}
+ get width() {
+   return this._width;
+ }
+
+ get height() {
+   return this._height;
+ }
+
+ getArea() {
+   return this._width * this._height;
+ }
+}
+
+class Square extends Rectangle {
+ constructor(side) {
+   super(side, side); // Call the Rectangle constructor with side as both width and height
+ }
+
+ getPerimeter() {
+   return 4 * this._width; // Access the inherited width property
+ }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
